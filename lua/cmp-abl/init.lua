7e425f317df4bef326bda1bfd8637582fc93e954
TechnicalDC <chauhandilip305@gmail.com>
2023-04-30 12:57:10 +0530

    updated

--- lua/cmp-abl/init.lua
+++ lua/cmp-abl/init.lua
@@ -1,80 +1,76 @@
 -- Custom nvim-cmp source for GitHub handles.
-local handles = {}
-
 local registered = false
 local has_keywords, keywords = pcall(require,"cmp-abl.keywords")
 
-handles.setup = function()
-	if registered then
-		return
-	end
-	registered = true
+if registered then
+	return
+end
+registered = true
 
-	local has_cmp, cmp = pcall(require, 'cmp')
+local has_cmp, cmp = pcall(require, 'cmp')
 
-	if not has_cmp then
-		return
-	end
+if not has_cmp then
+	return
+end
 
-	if not has_keywords then
-		return
-	end
+if not has_keywords then
+	return
+end
 
-	local source = {}
+local source = {}
 
-	source.new = function()
-		return setmetatable({}, {__index = source})
-	end
+source.new = function()
+	return setmetatable({}, {__index = source})
+end
 
-	source.get_trigger_characters = function()
-		return { ' ', '&', '{' }
-	end
+source.get_trigger_characters = function()
+	return { ' ', '&', '{' }
+end
 
-	source.get_keyword_pattern = function()
-		-- Add dot to existing keyword characters (\k).
-		-- return [[\%(\k\|\.\)\+]]
-	end
+source.get_keyword_pattern = function()
+	-- Add dot to existing keyword characters (\k).
+	-- return [[\%(\k\|\.\)\+]]
+end
 
-	local function validateInput(input, prefix)
-		for indx, char in ipairs(source.get_trigger_characters()) do
-			if (vim.startswith(input, char) or
-				vim.startswith(input, char) or
-				vim.startswith(input, char)) and
-				(prefix == char or
-				prefix == char or
-				prefix == char) then
-				return true
-			end
+local function validateInput(input, prefix)
+	for indx, char in ipairs(source.get_trigger_characters()) do
+		if (vim.startswith(input, char) or
+			vim.startswith(input, char) or
+			vim.startswith(input, char)) and
+			(prefix == char or
+			prefix == char or
+			prefix == char) then
+			return true
 		end
-		return false
 	end
+	return false
+end
 
-	source.complete = function(self, request, callback)
-		local input = string.sub(request.context.cursor_before_line, request.offset - 1)
-		local prefix = string.sub(request.context.cursor_before_line, 1, request.offset - 1)
+source.complete = function(self, request, callback)
+	local input = string.sub(request.context.cursor_before_line, request.offset - 1)
+	local prefix = string.sub(request.context.cursor_before_line, 1, request.offset - 1)
 
-		if validateInput(input, prefix) then
+	if validateInput(input, prefix) then
 		-- if vim.startswith(input, '@') and prefix == '@' then
-			local items = {}
-			for handle, address in pairs(keywords) do
-				table.insert(items, {
-					filterText = handle .. ' ' .. keywords,
-					label = keywords,
-					textEdit = {
-						newText = keywords,
-						range = {
-							start = {
-								line = request.context.cursor.row - 1,
-								character = request.context.cursor.col - 1 - #input,
-							},
-							['end'] = {
+		local items = {}
+		for handle, address in pairs(keywords) do
+			table.insert(items, {
+				filterText = handle .. ' ' .. keywords,
+				label = keywords,
+				textEdit = {
+					newText = keywords,
+					range = {
+						start = {
 							line = request.context.cursor.row - 1,
-							character = request.context.cursor.col - 1,
+							character = request.context.cursor.col - 1 - #input,
 						},
+						['end'] = {
+						line = request.context.cursor.row - 1,
+						character = request.context.cursor.col - 1,
 					},
 				},
-			}
-			)
+			},
+		})
 		end
 		callback {
 			items = items,
@@ -97,6 +93,5 @@ end
 		--     { name = 'handles' }, -- GitHub handles; eg. @wincent → Greg Hurrell <wincent@github.com>
 		--   }),
 		-- })
-	end
 
-	return handles
+return source
